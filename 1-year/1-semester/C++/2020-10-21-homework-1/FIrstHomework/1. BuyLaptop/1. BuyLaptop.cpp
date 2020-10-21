f367f7e33da41e44c57239df4997451a11a9cff8
ALMLAMBO <aleksm007148@gmail.com>
2020-10-21 20:37:56 +0300

    Add project for solving first problem

---
+++ 1-year/1-semester/C++/2020-10-21-homework-1/FIrstHomework/1. BuyLaptop/1. BuyLaptop.cpp
@@ -0,0 +1,21 @@
+/**
+*
+* Solution to homework assignment 1
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2020/2021
+*
+* @author Aleksander Marinov
+* @idnumber 62622
+* @task 1
+* @compiler VC
+*
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	return 0;
+}
