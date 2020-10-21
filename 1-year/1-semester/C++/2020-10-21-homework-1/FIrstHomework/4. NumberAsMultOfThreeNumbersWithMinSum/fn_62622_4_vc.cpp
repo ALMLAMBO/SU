941cea4a10816331874e464325abf60c6cdb2fe9
ALMLAMBO <aleksm007148@gmail.com>
2020-10-21 23:00:34 +0300

    Add project for presenting number as multiplication of three numbers, whose sum is minimum

---
+++ 1-year/1-semester/C++/2020-10-21-homework-1/FIrstHomework/4. NumberAsMultOfThreeNumbersWithMinSum/fn_62622_4_vc.cpp
@@ -0,0 +1,13 @@
+/**
+*
+* Solution to homework assignment 1
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2020/2021
+*
+* @author Aleksander Marinov
+* @idnumber 62622
+* @task 4
+* @compiler VC
+*
+*/
