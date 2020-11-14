1673857873ede1d61d078d976fa90db9a7e0f6d5
ALMLAMBO <aleksm007148@gmail.com>
2020-11-14 16:21:11 +0200

    Add project for finding count of three numbers whose sum is zero

---
+++ 1-year/1-semester/C++/2020-11-14-homework-2/Homework2/4. SumOfThreeNumbersEqualZero/4. SumOfThreeNumbersEqualZero.cpp
@@ -0,0 +1,22 @@
+/**
+*
+* Solution to homework assignment 2
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2020/2021
+*
+* @author Aleksander Marinov
+* @idnumber 62622
+* @task 4
+* @compiler VC
+*
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	return 0;
+}
