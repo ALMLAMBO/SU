d308f418d59c974f5a195204c8b17072e62ecb8b
ALMLAMBO <aleksm007148@gmail.com>
2020-12-20 12:14:48 +0200

    Add problem solution

--- 1-year/1-semester/C++/2020-12-05-homework-3/Homework3/4. SubstringCount/4. SubstringCount.cpp
+++ 1-year/1-semester/C++/2020-12-05-homework-3/Homework3/4. SubstringCount/4. SubstringCount.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int count_substrings(char* string);
-bool compare_strings(char* str1, char* str2);
-char* get_substring(char* string, int begin_index,
+int compare_strings(char* str1, char* str2);
+int string_length(char* string);
+int string_length(char const* string);
+int get_unique_letters_count(char* string);
+char* get_substring(char* string, int begin_index, 
 	int end_index, int removed_letters);
 
+char* convert_const_to_normal(char const* string);
 bool contains_only_letters(char* string);
-int factorial(int number);
 
 int main() {
 	char* string = new char[100];
@@ -45,41 +49,37 @@ int count_substrings(char* string) {
 	}
 
 	int max_letters_to_be_removed = length - 1;
+	int max_substrings = length * length * (length - 1);
+	char** substrings = new char* [max_substrings];
+	int sub_count = 0;
 
-	int len = factorial(length);
-	char** all_substrings = new char* [len];
-	int index = 0;
+	count += get_unique_letters_count(string);
 
-	for (int j = 1; j <= max_letters_to_be_removed; j++) {
+	for (int j = 1; j <= max_letters_to_be_removed - 1; j++) {
 		for (int i = 0; i < length; i++) {
-			char* str1 = get_substring(string, i, i + j, j);
-			all_substrings[index] = str1;
+			char* substring = get_substring(string, 
+				length - i - j, length - i, j);
 
-			index++;
+			substrings[sub_count++] = substring;
 		}
 	}
+	
+	//https://stackoverflow.com/questions/20931669/sort-a-2d-array-in-c-using-built-in-functionsor-any-other-method
+	qsort(substrings, sub_count, sizeof(*substrings),
+		[](const void* arg1, const void* arg2) -> int {
 
-	for (int i = 0; i < index - 1; i++) {
-		char* str1 = all_substrings[i];
-		char* str2 = all_substrings[i + 1];
+			char const* str1_const = static_cast<char const*>(arg1);
+			char const* str2_const = static_cast<char const*>(arg2);
+			char* str1 = convert_const_to_normal(str1_const);
+			char* str2 = convert_const_to_normal(str2_const);
 
-		int len1 = strlen(all_substrings[i]);
-		int len2 = strlen(all_substrings[i + 1]);
-		bool equal = true;
+			return compare_strings(str1, str2);
+	});
 
-		if (len1 > len2 || len2 > len1) {
-			count++;
-		}
-		else if (len1 == len2) {
-			for (int j = 0; j < len1; j++) {
-				if (str1[j] != str2[j]) {
-					equal = false;
-					break;
-				}
-			}
-		}
-
-		if (!equal) {
+	for (int i = 0; i < sub_count - 1; i++) {
+		if (compare_strings(substrings[i], 
+			substrings[i + 1]) == 0) {
+			
 			count++;
 		}
 	}
@@ -87,46 +87,87 @@ int count_substrings(char* string) {
 	return count;
 }
 
-bool compare_strings(char* str1, char* str2) {
-	if (str1 == NULL || str2 == NULL) {
-		return false;
+int compare_strings(char* str1, char* str2) {
+	int len_str1 = string_length(str1);
+	int len_str2 = string_length(str2);
+
+	if (len_str1 > len_str2) {
+		return 1;
 	}
-
-	int len_str1 = strlen(str1);
-	int len_str2 = strlen(str2);
-
-	if (len_str1 != len_str2) {
-		return false;
+	else if (len_str1 < len_str2) {
+		return -1;
 	}
-
 
 	for (int i = 0; i < len_str1; i++) {
-		if (str1[i] != str2[i]) {
-			return false;
+		if (str1[i] > str2[i]) {
+			return 1;
+		}
+		else if (str1[i] < str2[i]) {
+			return -1;
+		}
+	}
+	
+	return 0;
+}
+
+int string_length(char* string) {
+	int count = 0;
+	while (string[count++] != '\0');
+
+	return count - 1;
+}
+
+int string_length(char const* string) {
+	int count = 0;
+	while (string[count++] != '\0');
+
+	return count - 1;
+}
+
+int get_unique_letters_count(char* string) {
+	int count = 0;
+	int length = string_length(string);
+	sort(string, string + length);
+
+	for (int i = 0; i < length - 1; i++) {
+		if (string[i] != string[i + 1]) {
+			count++;
 		}
 	}
 
-	return true;
+	count++;
+
+	return count;
 }
 
 char* get_substring(char* string, int begin_index,
 	int end_index, int removed_letters) {
 
-	int substring_len = strlen(string) - removed_letters;
-	char* substring = new char[substring_len + 1];
+	int string_len = string_length(string);
+	int substring_length = string_len - removed_letters;
+	char* substring = new char[substring_length + 1];
 	int index = 0;
 
-	for (int i = 0; i < strlen(string); i++) {
-		if (i < begin_index || i >= end_index) {
-			substring[index] = string[i];
-			index++;
+	for (int i = 0; i < string_len; i++) {
+		if (i < begin_index || i > end_index) {
+			substring[index++] = string[i];
 		}
 	}
 
-	index++;
 	substring[index] = '\0';
 
 	return substring;
+}
+
+char* convert_const_to_normal(char const* string) {
+	int length = string_length(string);
+	char* result_string = new char[length];
+
+	for (int i = 0; i < length; i++) {
+		result_string[i] = string[i];
+	}
+
+	return result_string;
 }
 
 bool contains_only_letters(char* string) {
@@ -141,14 +182,4 @@ bool contains_only_letters(char* string) {
 	}
 
 	return true;
-}
-
-int factorial(int number) {
-	int result = 1;
-
-	for (int i = 2; i <= number; i++) {
-		result *= i;
-	}
-
-	return result;
 }
