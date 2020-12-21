a9e6083e21c6718c33b1a841ae9172ec2d5efc38
ALMLAMBO <aleksm007148@gmail.com>
2020-12-21 18:09:22 +0200

    Add file for functions implementations from helper_functions.h

---
+++ protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/helper_functions.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include "helper_functions.h"
+
+using namespace std;
+
+/// <summary>
+/// Calculates square root of number
+/// </summary>
+/// <param name="number">Input number</param>
+/// <returns>square root</returns>
+double calclulate_sqrt(double number) {
+	
+}
+
+/// <summary>
+/// pows number with exponent
+/// </summary>
+/// <param name="number">input number</param>
+/// <param name="exponent">input exponent</param>
+/// <returns>result after power</returns>
+double pow_number(double number, int exponent) {
+
+}
+
+/// <summary>
+/// Gets absolute value of number
+/// </summary>
+/// <param name="number">input number</param>
+/// <returns>absolute value</returns>
+double absolute_value(double number) {
+
+}
+
+/// <summary>
+/// Get matrix with values
+/// </summary>
+/// <param name="file">reference to input file stream</param>
+/// <param name="sizes">dimensions of the matrix</param>
+/// <returns>matrix with values</returns>
+double** get_matrix_values(ifstream& file,
+	 matrix_dimensions sizes) {
+
+
+}
+
+/// <summary>
+/// Gets length of number
+/// </summary>
+/// <param name="number">input number</param>
+/// <returns>how long it is</returns>
+int find_number_length(double number) {
+
+}
+
+/// <summary>
+/// Gets matrix dimensions
+/// </summary>
+/// <param name="file">reference input file stream</param>
+/// </returns>struct with matrix sizes<returns>
+struct matrix_dimensions get_matrix_dimensions(
+	ifstream& file) {
+
+
+}
+
+/// <summary>
+/// Gets matrix elements length
+/// </summary>
+/// <param name="matrix">struct with matrix values and dimensions</param>
+/// </returns>struct with matrix elements length<returns>
+struct matrix_elements_length get_matrix_elements_length(
+	struct matrix_representation matrix) {
+
+
+}
+
+/// <summary>
+/// Gets longest number
+/// </summary>
+/// <param name="elements">matrix elements length</param>
+/// <returns></returns>
+int get_longest_number_length(
+	struct matrix_elements_length elements) {
+
+
+}
