f47a8231a5de8c1a1c73efc368d27025df27bdf5
ALMLAMBO <aleksm007148@gmail.com>
2020-12-21 14:10:41 +0200

    Add header file with all required and optional operation with matrices

---
+++ protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/matrix_operations.h
@@ -0,0 +1,33 @@
+#ifndef __MATRIX_OPERATIONS__
+#define __MATRIX_OPERATIONS__
+
+struct maxtrix_dimesions {
+	int rows;
+	int columns;
+};
+
+struct matrix_representation {
+	double** values;
+	int rows;
+	int columns;
+};
+
+struct matrix_elements_length {
+	int* elements_length;
+	int elements_length_count;
+};
+
+//Required
+void matrix_multiplication_with_number();
+void matrices_multiplication();
+void calculate_matrix_determinant();
+void matrix_division_with_number();
+void find_matrix_inverse();
+void matrix_transposition();
+void singular_value_decomposition();
+
+//Optional
+void matrix_sum_substract(bool sum);
+void matrices_division();
+
+#endif // !__MATRIX_OPERATIONS__
