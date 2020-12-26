34f3eb654c7673d18305f8ddeeabe18e92320755
ALMLAMBO <aleksm007148@gmail.com>
2020-12-26 11:09:12 +0200

    Remove includes and namespace using and change return types and params types

--- protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/helper_functions.h
+++ protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/helper_functions.h
@@ -2,24 +2,22 @@
 #define __HELPER_FUNCTIONS__
 
 #include <fstream>
-#include "matrix_operations.h"
-using namespace std;
 
 double calclulate_sqrt(double number);
 double pow_number(double number, int exponent);
 double absolute_value(double number);
-double** get_matrix_values(ifstream& file,
-	struct matrix_dimensions sizes);
+double** get_matrix_values(std::ifstream& file,
+	struct matrix_dimensions * sizes);
 
 int find_number_length(double number);
 
-struct matrix_dimensions get_matrix_dimensions(
+struct matrix_dimensions * get_matrix_dimensions(
 	ifstream& file);
 
-struct matrix_elements_length get_matrix_elements_length(
-	struct matrix_representation matrix);
+struct matrix_elements_length * get_matrix_elements_length(
+	struct matrix_representation * matrix);
 
 int get_longest_number_length(
-	struct matrix_elements_length elements);
+	struct matrix_elements_length * elements);
 
 #endif // !__HELPER_FUNCTIONS__
