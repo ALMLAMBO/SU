8af18bb1c5e4653b5453e1637eb62167f671aff5
ALMLAMBO <aleksm007148@gmail.com>
2020-12-26 11:14:06 +0200

    Remove include statement and add std::

--- protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/helper_functions.h
+++ protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/helper_functions.h
@@ -1,8 +1,6 @@
 #ifndef __HELPER_FUNCTIONS__
 #define __HELPER_FUNCTIONS__
 
-#include <fstream>
-
 double calclulate_sqrt(double number);
 double pow_number(double number, int exponent);
 double absolute_value(double number);
@@ -12,7 +10,7 @@ double** get_matrix_values(std::ifstream& file,
 int find_number_length(double number);
 
 struct matrix_dimensions * get_matrix_dimensions(
-	ifstream& file);
+	std::ifstream& file);
 
 struct matrix_elements_length * get_matrix_elements_length(
 	struct matrix_representation * matrix);
