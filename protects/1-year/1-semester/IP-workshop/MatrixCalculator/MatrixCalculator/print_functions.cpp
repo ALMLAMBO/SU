0486e6752557367f5644191010c5893903dd13f1
ALMLAMBO <aleksm007148@gmail.com>
2020-12-30 21:17:33 +0200

    [UP-project] Add implementation file for print_functions.h header file

---
+++ protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/print_functions.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "print_functions.h"
+#include "matrix_operations.h"
+
+using namespace std;
+
+/// <summary>
+/// Prints available operations with matrices
+/// </summary>
+void print_available_operations() {
+	
+}
+
+/// <summary>
+/// Prints matrix
+/// </summary>
+/// <param name="matrix">input matrix</param>
+void print_matrix(struct matrix_representation* matrix) {
+
+}
+
+/// <summary>
+/// Prints result matrix after mult or division with number
+/// </summary>
+/// <param name="matrix">result matrix</param>
+/// <param name="scalar">scalar value</param>
+/// <param name="multiplication">true for mult, false for division</param>
+void print_matrix_multiplication_division_with_number(
+	struct matrix_representation* matrix,
+	double scalar, bool multiplication) {
+
+
+}
+
+/// <summary>
+/// Prints result matrix after matrices mult or division
+/// </summary>
+/// <param name="first_matrix">first matrix</param>
+/// <param name="second_matrix">second matrix</param>
+/// <param name="division">true for division, false for multiplication</param>
+void print_matrices_multiplication_division(
+	struct matrix_representation first_matrix,
+	struct matrix_representation second_matrix,
+	bool division) {
+
+
+}
