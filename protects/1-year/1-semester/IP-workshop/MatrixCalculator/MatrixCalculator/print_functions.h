797421b4d2ac446d4aa77f1d2e420a672c4b3a9e
ALMLAMBO <aleksm007148@gmail.com>
2020-12-30 21:02:27 +0200

    [UP-project] Add function for printing available commands and change param type

--- protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/print_functions.h
+++ protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/print_functions.h
@@ -1,10 +1,11 @@
 #ifndef __PRINT_FUNCTIONS__
 #define __PRINT_FUNCTIONS__
 
-void print_matrix(struct matrix_representation matrix);
+void print_available_operations();
+void print_matrix(struct matrix_representation * matrix);
 
 void print_matrix_multiplication_division_with_number(
-	double** matrix, struct maxtrix_dimesions dimensions,
+	struct matrix_representation* matrix,
 	double scalar, bool multiplication);
 
 void print_matrices_multiplication_division(
