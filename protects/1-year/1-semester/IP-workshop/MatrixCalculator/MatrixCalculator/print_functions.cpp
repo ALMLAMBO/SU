76d7ac23dc980b9ececec6235d3db9a1161df327
ALMLAMBO <aleksm007148@gmail.com>
2020-12-30 21:20:17 +0200

    [UP-project] Implement function for printing operations with matrices

--- protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/print_functions.cpp
+++ protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/print_functions.cpp
@@ -8,7 +8,16 @@ using namespace std;
 /// Prints available operations with matrices
 /// </summary>
 void print_available_operations() {
-	
+	cout << "1. Matrix multiplication with number" << endl;
+	cout << "2. Matrices multiplication" << endl;
+	cout << "3. Find matrix determinant" << endl;
+	cout << "4. Matrix division with number" << endl;
+	cout << "5. Find inverse matrix" << endl;
+	cout << "6. Matrix transpose" << endl;
+	cout << "7. Singular value decomposition (SVD)" << endl;
+	cout << "(Optional)8. Matrices sum" << endl;
+	cout << "(Optional)9. Matrices subtraction" << endl;
+	cout << "(Optional)10. Matrices division" << endl;
 }
 
 /// <summary>
