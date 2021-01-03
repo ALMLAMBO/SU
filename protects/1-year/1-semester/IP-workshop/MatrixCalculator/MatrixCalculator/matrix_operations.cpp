b607adebb5faa7b18e90ec007cbec468693353e8
ALMLAMBO <aleksm007148@gmail.com>
2021-01-04 00:42:24 +0200

    [UP-project] Implement function for managing matrix operations

--- protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/matrix_operations.cpp
+++ protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/matrix_operations.cpp
@@ -6,3 +6,30 @@
 #include "print_functions.h"
 
 using namespace std;
+
+/// <summary>
+/// Manages matrix operations
+/// </summary>
+/// <param name="filename">file to read</param>
+/// <param name="option">integer for which operation to choose</param>
+void matrix_operations_manager(const char* filename, int option) {
+	ifstream file;
+	file.open(filename);
+
+	if (file.is_open()) {
+		switch (option) {
+			case 1: 
+				matrix_multiplication_with_number(filename); 
+				break;
+
+			default:
+				cout << "Invalid operation" << endl;
+				break;
+		}
+	}
+	else {
+		cout << "File not opened successfully" << endl;
+
+		return;
+	}
+}
