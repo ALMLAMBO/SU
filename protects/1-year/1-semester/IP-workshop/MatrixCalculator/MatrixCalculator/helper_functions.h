814a83ea08a3229ea40db8a737eac2ba0baf0997
ALMLAMBO <aleksm007148@gmail.com>
2021-01-04 16:35:26 +0200

    [UP-project] Add function for getting matrix dimensions

--- protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/helper_functions.h
+++ protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/helper_functions.h
@@ -5,4 +5,6 @@ double calclulate_sqrt(double number);
 double pow_number(double number, int exponent);
 double absolute_value(double number);
 
+MatrixDimensions& get_matrix_dimensions(ifstream& file);
+
 #endif // !__HELPER_FUNCTIONS__
