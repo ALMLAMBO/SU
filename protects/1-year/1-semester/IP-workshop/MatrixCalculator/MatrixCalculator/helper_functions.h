cb2c1c3c318c100821d7c410e0c7803ab0eb052a
ALMLAMBO <aleksm007148@gmail.com>
2021-01-04 17:25:47 +0200

    [UP-project] Add includes and change param type

--- protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/helper_functions.h
+++ protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/helper_functions.h
@@ -1,10 +1,12 @@
 #ifndef __HELPER_FUNCTIONS__
 #define __HELPER_FUNCTIONS__
+#include <fstream>
+#include "matrix_operations.h"
 
 double calclulate_sqrt(double number);
 double pow_number(double number, int exponent);
 double absolute_value(double number);
 
-MatrixDimensions& get_matrix_dimensions(ifstream& file);
+MatrixDimensions& get_matrix_dimensions(std::ifstream& file);
 
 #endif // !__HELPER_FUNCTIONS__
