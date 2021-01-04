1aa9e87c49117d705f363fb8e4dfac2e10fe2aa3
ALMLAMBO <aleksm007148@gmail.com>
2021-01-04 17:53:32 +0200

    [UP-project] Add functions for getting matrix values, matrix and scalar

--- protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/helper_functions.h
+++ protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/helper_functions.h
@@ -8,5 +8,10 @@ double pow_number(double number, int exponent);
 double absolute_value(double number);
 
 MatrixDimensions get_matrix_dimensions(std::ifstream& file);
+double** get_matrix_values(std::ifstream& file, 
+	MatrixDimensions dimensions);
+
+MatrixRepresentation get_matrix(std::ifstream file);
+double get_scalar(std::ifstream& file);
 
 #endif // !__HELPER_FUNCTIONS__
