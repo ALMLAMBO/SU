8b6470681713635adb7e2b452c6f3a0d904b31f6
ALMLAMBO <aleksm007148@gmail.com>
2021-01-04 17:54:17 +0200

    [UP-project] Add function for printing matrix

--- protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/print_functions.h
+++ protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/print_functions.h
@@ -1,6 +1,9 @@
 #ifndef __PRINT_FUNCTIONS__
 #define __PRINT_FUNCTIONS__
 
+#include "matrix_operations.h"
+
 void print_available_operations();
+void print_matrix(MatrixRepresentation& matrix);
 
 #endif // !__PRINT_FUNCTIONS__
