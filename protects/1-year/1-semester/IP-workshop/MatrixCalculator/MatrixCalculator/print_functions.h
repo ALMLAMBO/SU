db4c6ea92328dc14558950a36d6ae27a6df87904
ALMLAMBO <aleksm007148@gmail.com>
2021-01-09 20:07:38 +0200

    [UP-project] Remove function for printing matrix and add functions for printing operations with matrices and matrix with number

--- protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/print_functions.h
+++ protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/print_functions.h
@@ -4,6 +4,16 @@
 #include "matrix_operations.h"
 
 void print_available_operations();
-void print_matrix(MatrixRepresentation& matrix);
+void print_matrices_operations(
+	MatrixRepresentation first_input_matrix,
+	MatrixRepresentation second_input_matrix,
+	MatrixRepresentation result_matrix,
+	char operation_symbol);
+
+void print_matrix_mult_divide_number(
+	MatrixRepresentation input_matrix,
+	MatrixRepresentation result_matrix,
+	char operation_symbol,
+	const char* message);
 
 #endif // !__PRINT_FUNCTIONS__
