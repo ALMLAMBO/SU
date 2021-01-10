c9647494259b08062037b22cf1c288ae359d9004
ALMLAMBO <aleksm007148@gmail.com>
2021-01-10 21:50:36 +0200

    Add comment block

--- protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/MatrixCalculator.cpp
+++ protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/MatrixCalculator.cpp
@@ -1,3 +1,19 @@
+/**
+*
+* Solution to course project # 6
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2020/2021
+*
+* @author Aleksander Marinov
+* @idnumber 62622
+* @compiler VC
+*
+* <main file>
+*
+*/
+
+
 #include <iostream>
 #include "print_functions.h"
 #include "matrix_operations.h"
--- protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/print_functions.cpp
+++ protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/print_functions.cpp
@@ -1,3 +1,18 @@
+/**
+*
+* Solution to course project # 6
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2020/2021
+*
+* @author Aleksander Marinov
+* @idnumber 62622
+* @compiler VC
+*
+* <file with print functions>
+*
+*/
+
 #include <iostream>
 #include <algorithm>
 #include <iomanip>
