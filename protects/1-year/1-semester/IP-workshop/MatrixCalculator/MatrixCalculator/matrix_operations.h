a41fab7a4f4669fb00e8cf6477cbfdec0e5eeddc
ALMLAMBO <aleksm007148@gmail.com>
2021-01-19 17:27:09 +0200

    [UP-project] Make multiplication and division with number in one method

--- protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/matrix_operations.h
+++ protects/1-year/1-semester/IP-workshop/MatrixCalculator/MatrixCalculator/matrix_operations.h
@@ -17,99 +17,99 @@
 #define __MATRIX_OPERATIONS__
 
 struct MatrixDimensions {
-	private:
-		int rows_;
-		int columns_;
+private:
+	int rows_;
+	int columns_;
 
-	public:
-		void set_rows(int rows) {
-			rows_ = rows;
-		}
+public:
+	void set_rows(int rows) {
+		rows_ = rows;
+	}
 
-		int get_rows() const {
-			return rows_;
-		}
+	int get_rows() const {
+		return rows_;
+	}
 
-		void set_columns(int columns) {
-			columns_ = columns;
-		}
+	void set_columns(int columns) {
+		columns_ = columns;
+	}
 
-		int get_columns() const {
-			return columns_;
-		}
+	int get_columns() const {
+		return columns_;
+	}
 
-		MatrixDimensions operator=(const MatrixDimensions& dimens) {
-			MatrixDimensions sizes;
-			
-			sizes.set_rows(dimens.get_rows());
-			sizes.set_columns(dimens.get_columns());
+	MatrixDimensions operator=(const MatrixDimensions& dimens) {
+		MatrixDimensions sizes;
 
-			return sizes;
-		}
+		sizes.set_rows(dimens.get_rows());
+		sizes.set_columns(dimens.get_columns());
+
+		return sizes;
+	}
 };
 
 struct MatrixRepresentation {
-	private: 
-		MatrixDimensions dimensions_;
-		double** values_;
+private:
+	MatrixDimensions dimensions_;
+	double** values_;
 
-	public:
-		MatrixRepresentation() : 
-			dimensions_(), values_(NULL) { }
-		
-		MatrixRepresentation(MatrixDimensions& dimensions) : 
-			dimensions_(dimensions), values_(NULL) { }
+public:
+	MatrixRepresentation() :
+		dimensions_(), values_(NULL) { }
 
-		void set_dimensions(MatrixDimensions& dimensions) {
-			dimensions_.set_rows(dimensions.get_rows());
-			dimensions_.set_columns(dimensions.get_columns());
-		}
+	MatrixRepresentation(MatrixDimensions& dimensions) :
+		dimensions_(dimensions), values_(NULL) { }
 
-		MatrixDimensions get_dimensions() const {
-			return dimensions_;
-		}
+	void set_dimensions(MatrixDimensions& dimensions) {
+		dimensions_.set_rows(dimensions.get_rows());
+		dimensions_.set_columns(dimensions.get_columns());
+	}
 
-		void set_values(double ** values) {
-			const int ROWS = dimensions_.get_rows();
-			const int COLUMNS = dimensions_.get_columns();
+	MatrixDimensions get_dimensions() const {
+		return dimensions_;
+	}
 
-			for (int i = 0; i < ROWS; i++) {
-				for (int j = 0; j < COLUMNS; j++) {
-					values_[i][j] = values[i][j];
-				}
+	void set_values(double** values) {
+		const int ROWS = dimensions_.get_rows();
+		const int COLUMNS = dimensions_.get_columns();
+
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLUMNS; j++) {
+				values_[i][j] = values[i][j];
 			}
 		}
+	}
 
-		double** get_values() const {
-			return values_;
+	double** get_values() const {
+		return values_;
+	}
+
+	void init_empty_matrix_values() {
+		const int ROWS = dimensions_.get_rows();
+		const int COLUMNS = dimensions_.get_columns();
+
+		values_ = new double* [ROWS];
+
+		for (int i = 0; i < ROWS; i++) {
+			values_[i] = new double[COLUMNS];
+		}
+	}
+
+	void destroy_matrix_values() {
+		const int ROWS = dimensions_.get_rows();
+
+		for (int i = 0; i < ROWS; i++) {
+			delete[] values_[i];
 		}
 
-		void init_empty_matrix_values() {
-			const int ROWS = dimensions_.get_rows();
-			const int COLUMNS = dimensions_.get_columns();
-
-			values_ = new double* [ROWS];
-
-			for (int i = 0; i < ROWS; i++) {
-				values_[i] = new double[COLUMNS];
-			}
-		}
-
-		void destroy_matrix_values() {
-			const int ROWS = dimensions_.get_rows();
-
-			for (int i = 0; i < ROWS; i++) {
-				delete[] values_[i];
-			}
-
-			delete[] values_;
-			values_ = NULL;
-		}
+		delete[] values_;
+		values_ = NULL;
+	}
 };
 
 //Required
-MatrixRepresentation matrix_multiplication_with_number(
-	const char * filename);
+MatrixRepresentation matrix_mult_divide_with_number(
+	const char* filename, bool mult);
 
 MatrixRepresentation matrices_multiplication(
 	MatrixRepresentation first_matrix,
@@ -118,19 +118,11 @@ MatrixRepresentation matrices_multiplication(
 double calculate_matrix_determinant(
 	MatrixRepresentation matrix);
 
-MatrixRepresentation matrix_division_with_number(
-	MatrixRepresentation matrix, double scalar);
-
 MatrixRepresentation find_matrix_inverse(
 	MatrixRepresentation matrix);
 
 MatrixRepresentation matrix_transposition(
 	MatrixRepresentation matrix);
-//
-////Optional
-//void matrix_sum_substract(bool sum);
-//void matrices_division();
-//void singular_value_decomposition();
 
 void matrix_operations_manager(int option);
 
