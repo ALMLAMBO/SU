01a57174c118f8b2ccc2565e0ec6e8efee156e30
ALMLAMBO <aleksm007148@gmail.com>
2021-03-06 10:41:30 +0200

    Add problem solution

--- 1-year/2-semester/OOP/2021-03-05-workshop-1/OOP-workshop-1-intro/Problem-1/Problem-1.cpp
+++ 1-year/2-semester/OOP/2021-03-05-workshop-1/OOP-workshop-1-intro/Problem-1/Problem-1.cpp
@@ -3,5 +3,35 @@
 using namespace std;
 
 int main() {
+	int rows, columns, number;
+
+	cin >> rows >> columns >> number;
+	int** matrix = new int* [rows];
+	
+	cout << endl;
+	for (int i = 0; i < rows; i++) {
+		matrix[i] = new int[columns];
+
+		for (int j = 0; j < columns; j++) {
+			if (i == j) {
+				matrix[i][j] = number;
+			}
+			else {
+				matrix[i][j] = 0;
+			}
+
+			cout << matrix[i][j] << " ";
+		}
+
+		cout << endl;
+	}
+
+	for (int i = 0; i < rows; i++) {
+		delete[] matrix[i];
+	}
+
+	delete[] matrix;
+	matrix = NULL;
+	
 	return 0;
 }
