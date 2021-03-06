f7eda0ca591c475b64c64e7b5843b7b727dc9fa1
ALMLAMBO <aleksm007148@gmail.com>
2021-03-06 11:23:11 +0200

    Add problem solution

--- 1-year/2-semester/OOP/2021-03-05-workshop-1/OOP-workshop-1-intro/Problem-2/Problem-2.cpp
+++ 1-year/2-semester/OOP/2021-03-05-workshop-1/OOP-workshop-1-intro/Problem-2/Problem-2.cpp
@@ -2,6 +2,28 @@
 
 using namespace std;
 
+class Snake {
+private:
+	int x, y;
+
+public:
+	void set_x(int new_x) {
+		this->x = new_x;
+	}
+
+	void set_y(int new_y) {
+		this->y = new_y;
+	}
+
+	int get_x() const {
+		return this->x;
+	}
+
+	int get_y() const {
+		return this->y;
+	}
+};
+
 int main() {
 	return 0;
 }
