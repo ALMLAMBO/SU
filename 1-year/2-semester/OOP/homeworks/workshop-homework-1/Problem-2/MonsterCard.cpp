19c2e93a65b11ce8f534e8e531649b3c703e167e
ALMLAMBO <aleksm007148@gmail.com>
2021-03-20 12:57:30 +0200

    Add class for monster card with implementation

---
+++ 1-year/2-semester/OOP/homeworks/workshop-homework-1/Problem-2/MonsterCard.cpp
@@ -0,0 +1,63 @@
+#include "MonsterCard.hpp"
+
+MonsterCard::MonsterCard() : 
+	attack_points(0), defense_points(0) {
+
+	this->name = new char[1]{ '\0' };
+}
+
+MonsterCard::MonsterCard(const char*& name,
+	const unsigned int& attack_points,
+	const unsigned int& defense_points) {
+
+	this->attack_points = attack_points;
+	this->defense_points = defense_points;
+	this->name = HelperFunctions::copy_char_array(name, 25);
+}
+
+MonsterCard::MonsterCard(const MonsterCard& from) {
+	this->copy_monster_card(from);
+}
+
+MonsterCard::~MonsterCard() {
+	delete this->name;
+}
+
+MonsterCard& MonsterCard::operator=(const MonsterCard& other) {
+	if (this != &other) {
+		delete[] this->name;
+		this->copy_monster_card(other);
+	}
+
+	return *this;
+}
+
+void MonsterCard::set_name(const char*& new_name) {
+	this->name = HelperFunctions::copy_char_array(new_name, 25);
+}
+
+void MonsterCard::set_attack_points(const int& new_attack_points) {
+	this->attack_points = new_attack_points;
+}
+
+void MonsterCard::set_defense_points(const int& new_defense_points) {
+	this->defense_points = new_defense_points;
+}
+
+char* MonsterCard::get_name() const {
+	return this->name;
+}
+
+unsigned int MonsterCard::get_attack_points() const {
+	return this->attack_points;
+}
+
+unsigned int MonsterCard::get_defense_points() const {
+	return this->defense_points;
+}
+
+void MonsterCard::copy_monster_card(const MonsterCard& other) {
+	this->attack_points = other.attack_points;
+	this->defense_points = other.defense_points;
+	this->name = HelperFunctions::copy_char_array(other.name, 25);
+}
---
+++ 1-year/2-semester/OOP/homeworks/workshop-homework-1/Problem-2/MonsterCard.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef __MONSTER_CARD_HPP__
+#define __MONSTER_CARD_HPP__
+
+#include "HelperFunctions.hpp"
+
+class MonsterCard {
+public:
+	MonsterCard();
+	MonsterCard(const char*& name,
+		const unsigned int& attact_points,
+		const unsigned int& defense_points);
+
+	MonsterCard(const MonsterCard& from);
+	~MonsterCard();
+	MonsterCard& operator=(const MonsterCard& other);
+
+	void set_name(const char*& new_name);
+	void set_attack_points(const int& new_attack_points);
+	void set_defense_points(const int& new_defense_points);
+
+	char* get_name() const;
+	unsigned int get_attack_points() const;
+	unsigned int get_defense_points() const;
+
+private:
+	char* name;
+	unsigned int attack_points;
+	unsigned int defense_points;
+
+	void copy_monster_card(const MonsterCard& other);
+};
+
+#endif // !__MONSTER_CARD_HPP__
