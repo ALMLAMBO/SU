f9be2519b3de06d99621b7088716e2294b98ddf3
ALMLAMBO <aleksm007148@gmail.com>
2021-03-20 13:24:43 +0200

    Implement class

--- 1-year/2-semester/OOP/homeworks/workshop-homework-1/Problem-2/MagicCard.cpp
+++ 1-year/2-semester/OOP/homeworks/workshop-homework-1/Problem-2/MagicCard.cpp
@@ -1,51 +1,65 @@
 #include "MagicCard.hpp"
 
 MagicCard::MagicCard() {
-
+	this->name = new char[] {'\0'};
+	this->effect = new char[] {'\0'};
+	this->type = MagicCardType::spell;
 }
 
 MagicCard::MagicCard(const char* name, const char* effect,
 	const MagicCardType& type) {
 
-
+	this->name = HelperFunctions::copy_char_array(name, 25);
+	this->effect = HelperFunctions::copy_char_array(effect, 100);
+	this->type = type;
 }
 
 MagicCard::MagicCard(const MagicCard& from) {
-
+	this->copy_magic_card(from);
 }
 
 MagicCard::~MagicCard() {
-
+	delete[] this->name;
+	delete[] this->effect;
 }
 
 MagicCard& MagicCard::operator=(const MagicCard& other) {
+	if (this != &other) {
+		delete[] this->name;
+		delete[] this->effect;
 
+		this->copy_magic_card(other);
+	}
+
+	return *this;
 }
 
 void MagicCard::set_name(const char* new_name) {
-
+	this->name = HelperFunctions::copy_char_array(new_name, 25);
 }
 
 void MagicCard::set_effect(const char* new_effect) {
-
+	this->effect = HelperFunctions::copy_char_array(new_effect, 100);
 }
 
 void MagicCard::set_type(const MagicCardType& new_type) {
-
+	this->type = new_type;
 }
 
 char* MagicCard::get_name() const {
-
+	return this->name;
 }
 
 char* MagicCard::get_effect() const {
-
+	return this->effect;
 }
 
 MagicCardType MagicCard::get_type() const {
-
+	return this->type;
 }
 
 void MagicCard::copy_magic_card(const MagicCard& other) {
-
+	this->name = HelperFunctions::copy_char_array(other.name, 25);
+	this->effect = HelperFunctions::copy_char_array(other.effect, 100);
+	this->type = other.type;
 }
